51f03395cbeb73c46c3be6a7ee6cd17d923811c9
@hexfa <amir.dehdarian2009@gmail.com>
2023-06-26 17:03:45 +0330

    Create app structure and basic setting for hilt and navigation component.

---
+++ app/src/main/java/com/cryptocurrency/hilt/HiltApplication.java
@@ -0,0 +1,9 @@
+package com.cryptocurrency.hilt;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HiltApplication extends Application {
+}
