9bf3981cad284be2920fe6e22719351796b92d26
@hexfa <amir.dehdarian2009@gmail.com>
2023-06-27 14:34:22 +0330

    Complete ProfileFragment.

--- app/src/main/java/com/cryptocurrency/fragment/ProfileFragment.java
+++ app/src/main/java/com/cryptocurrency/fragment/ProfileFragment.java
@@ -1,66 +1,190 @@
 package com.cryptocurrency.fragment;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.cryptocurrency.MainActivity;
 import com.cryptocurrency.R;
+import com.cryptocurrency.databinding.FragmentProfileBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.ByteArrayOutputStream;
+
+import me.ibrahimsn.lib.SmoothBottomBar;
+
+
 public class ProfileFragment extends Fragment {
+    FragmentProfileBinding fragmentProfileBinding;
+    MainActivity mainActivity;
+    SmoothBottomBar smoothBottomBar;
+    String picturePath;
+    String ImgFromStore;
+    String fName,lName,email;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ActivityResultLauncher<Intent> GetContext = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK){
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri selectImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        Cursor cursor = requireActivity().getContentResolver().query(selectImage,filePathColumn,null,null,null);
+                        cursor.moveToFirst();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        picturePath = cursor.getString(columnIndex);
+                        cursor.close();
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
+                        fragmentProfileBinding.roundedImageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    }
+                }
+            });
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        fragmentProfileBinding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.fragment_profile,container,false);
+        readToDataStore();
+        setDefaultValue();
+        getPhotoFromGallery();
+        setupSaveBtn();
+        return fragmentProfileBinding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //bottom navigation view becomes Gone
+        smoothBottomBar = requireActivity().findViewById(R.id.bottomBar);
+        smoothBottomBar.setVisibility(View.GONE);
+
+        setupToolbar(view);
+    }
+    private void setupToolbar(View view) {
+        NavController navController = Navigation.findNavController(view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.profileFragment).build();
+        Toolbar profileToolbar = view.findViewById(R.id.profiletoolbar);
+        NavigationUI.setupWithNavController(profileToolbar,navController,appBarConfiguration);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.profileFragment){
+                profileToolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+                profileToolbar.setTitle("Profile");
+            }
+        });
+    }
+
+    private void getPhotoFromGallery(){
+        fragmentProfileBinding.changeImg.setOnClickListener(view -> {
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},2000);
+            }else {
+                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                GetContext.launch(cameraIntent);
+            }
+        });
+    }
+
+    private void setupSaveBtn(){
+        fragmentProfileBinding.saveBtn.setOnClickListener(view -> {
+            writeToDataStore();
+            Snackbar.make(fragmentProfileBinding.ProfileCon,"picture saved",Snackbar.LENGTH_SHORT).show();
+        });
+    }
+
+    private void readToDataStore(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireActivity());
+        ImgFromStore = sharedPreferences.getString("profileImg",null);
+        fName = sharedPreferences.getString("firstName","");
+        lName = sharedPreferences.getString("lastName","");
+        email = sharedPreferences.getString("email","");
+    }
+
+    private void writeToDataStore(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireActivity());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if (picturePath !=null){
+            editor.putString("profileImg",encodeTobase64(BitmapFactory.decodeFile(picturePath)));
+        }
+        editor.putString("firstName",fragmentProfileBinding.editTextTextPersonName.getText().toString());
+        editor.putString("lastName",fragmentProfileBinding.editTextTextPersonName2.getText().toString());
+        editor.putString("email",fragmentProfileBinding.editTextTextPersonName3.getText().toString());
+        editor.apply();
+    }
+
+
+    public static String encodeTobase64(Bitmap bitmap){
+        Bitmap image = bitmap;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG,100,baos);
+        byte[] b = baos.toByteArray();
+        return Base64.encodeToString(b, Base64.DEFAULT);
+    }
+
+
+    public static Bitmap decodeBase64(String string){
+        byte[] decodeByte = Base64.decode(string,0);
+        return BitmapFactory.decodeByteArray(decodeByte,0,decodeByte.length);
+    }
+
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+    }
+
+    //when destroy this fragment, bottom navigation becomes visible
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        smoothBottomBar.setVisibility(View.VISIBLE);
+    }
+    private void setDefaultValue(){
+        if (ImgFromStore == null){
+            fragmentProfileBinding.roundedImageView.setImageResource(R.drawable.profile_placeholder);
+        }else {
+            fragmentProfileBinding.roundedImageView.setImageBitmap(decodeBase64(ImgFromStore));
+        }
+        fragmentProfileBinding.editTextTextPersonName.setText(fName);
+        fragmentProfileBinding.editTextTextPersonName2.setText(lName);
+        fragmentProfileBinding.editTextTextPersonName3.setText(email);
     }
 }
+
